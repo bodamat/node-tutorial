393b480fb8fed244583f4098a2365422c25447ef
Bohdan <bomtvv@gmail.com>
2021-08-17 16:34:11 +0300

    simple web-server

---
+++ web-server/src/app.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
