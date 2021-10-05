3d5c1bc7a82489538ff20d32355d969a8490599a
Bohdan <bomtvv@gmail.com>
2021-10-05 16:02:26 +0300

    add dynamic templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,36 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
 app.set('view engine', 'hbs');
-app.use(express.static(publicDirPath))
+app.set('views', viewsPath);
+app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Bohdan Matviiv'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Bohdan Matviiv'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Message that should help you.',
+        name: 'Bohdan Matviiv'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
