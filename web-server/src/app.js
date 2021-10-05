f6ed0d7517b25e43500531786264ef9331b35fde
Bohdan <bomtvv@gmail.com>
2021-10-05 17:18:14 +0300

    hbs partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
