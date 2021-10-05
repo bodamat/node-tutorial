d74823ed731b7e5ad64b4b58b5a1c487e63d3621
Bohdan <bomtvv@gmail.com>
2021-10-05 18:28:50 +0300

    error pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+   res.render('error', {
+       title: "404",
+       message: "Help article not found",
+       name: "Bohdan Matviiv"
+   })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: "404",
+        message: "Page not found",
+        name: "Bohdan Matviiv"
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
