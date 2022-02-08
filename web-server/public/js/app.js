71941348d3ac84d9b78534464a070e0078a40825
Bohdan <bomtvv@gmail.com>
2022-02-08 16:33:11 +0200

    finish weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,3 +3,28 @@
  */
 
 console.log('Client side javascript file is loaded!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('p#message-1');
+const messageTwo = document.querySelector('p#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then(res => {
+        res.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                return
+            }
+
+            messageOne.textContent = `Location: ${data.location}`
+            messageTwo.textContent = `Forecast: ${data.forecast}`;
+        })
+    }).catch(err => {
+        console.error(err);
+    })
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -45,7 +45,7 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: "You must provide a address"
+            error: "You must provide an address"
         })
     }
 
